4ef4ad8894ffb6c2591ed0f7833294dc5ae24243
tama555 <yoshi4548797@yahoo.co.jp>
2020-11-29 22:31:30 +0900

    a

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,7 +7,13 @@ class RentalsController < ApplicationController
   def create
     @rental = Rental.new(rental_params)
     @rental.update(loan_datetime: DateTime.current)
-    @device = Device.update(lending_status: 0)
+    device_id = @rental.device_id
+    if device_id == 1
+      flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
+      render :new
+    end
+    @devices = Device.where(id: device_id)
+    @device = @devices.update(lending_status: 1)
     if @rental.save
       redirect_to root_path
     else
