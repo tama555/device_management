0198e13787c83fd6252e1aae8fe53507069881cf
tama555 <yoshi4548797@yahoo.co.jp>
2020-12-10 20:07:34 +0900

    a

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,21 +6,46 @@ class RentalsController < ApplicationController
 
   def create
     @rental = Rental.new(rental_params)
-    @rental.update(loan_datetime: DateTime.current)
     device_id = @rental.device_id
-    @devices = Device.find(device_id)
-    if @devices.lending_status == true
-      flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
-      render :new
-    else
-      @device = @devices.update(lending_status: 1)
-      if @rental.save
-        redirect_to root_path
-      else
+    if Device.find_by(id: device_id)
+      @devices = Device.find(device_id)
+      if @devices.lending_status == true
+        flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
         render :new
+      else
+        @rental.update(loan_datetime: DateTime.current)
+        @device = @devices.update(lending_status: 1)
+        if @rental.save
+          redirect_to root_path
+        else
+          render :new
+        end
       end
+
+    else
+      flash.now[:alert]= '入力された機器IDは存在しません'
+      render :new
     end
+    
   end
+
+
+  #   @rental = Rental.new(rental_params)
+  #   @rental.update(loan_datetime: DateTime.current)
+  #   device_id = @rental.device_id
+  #   @devices = Device.find(device_id)
+  #   if @devices.lending_status == true
+  #     flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
+  #     render :new
+  #   else
+  #     @device = @devices.update(lending_status: 1)
+  #     if @rental.save
+  #       redirect_to root_path
+  #     else
+  #       render :new
+  #     end
+  #   end
+  # end
 
   def return
 
--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,2 +1,40 @@
 class ReturnsController < ApplicationController
+  def new
+    @return = Return.new
+  end
+
+  def create
+    @return = Return.new(return_params)
+    
+
+
+
+
+    device_id = @rental.device_id
+    if Device.find_by(id: device_id)
+      @devices = Device.find(device_id)
+      if @devices.lending_status == true
+        flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
+        render :new
+      else
+        @rental.update(loan_datetime: DateTime.current)
+        @device = @devices.update(lending_status: 1)
+        if @rental.save
+          redirect_to root_path
+        else
+          render :new
+        end
+      end
+
+    else
+      flash.now[:alert]= '入力された機器IDは存在しません'
+      render :new
+    end
+    
+  end
+
+  private
+  def return_params
+    params.require(:return).permit(:device_id, :return_staff, :department_id)
+  end
 end
