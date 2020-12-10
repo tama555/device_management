fdfc88e5a23490c8524bb0c69623867fc17fbde1
tama555 <yoshi4548797@yahoo.co.jp>
2020-12-10 21:21:47 +0900

    a

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -5,21 +5,18 @@ class ReturnsController < ApplicationController
 
   def create
     @return = Return.new(return_params)
-    
-
-
-
-
-    device_id = @rental.device_id
+    device_id = @return.device_id
     if Device.find_by(id: device_id)
       @devices = Device.find(device_id)
-      if @devices.lending_status == true
-        flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
+
+      if @devices.lending_status == false
+        flash.now[:alert]= '入力された機器IDは返却されているので登録できません'
         render :new
       else
-        @rental.update(loan_datetime: DateTime.current)
-        @device = @devices.update(lending_status: 1)
-        if @rental.save
+        @return.update(return_datetime: DateTime.current)
+        @device = @devices.update(lending_status: 0)
+
+        if @return.save
           redirect_to root_path
         else
           render :new
@@ -30,6 +27,30 @@ class ReturnsController < ApplicationController
       flash.now[:alert]= '入力された機器IDは存在しません'
       render :new
     end
+
+
+
+
+    # device_id = @rental.device_id
+    # if Device.find_by(id: device_id)
+    #   @devices = Device.find(device_id)
+    #   if @devices.lending_status == true
+    #     flash.now[:alert]= '入力された機器IDは貸出されているので登録できません'
+    #     render :new
+    #   else
+    #     @rental.update(loan_datetime: DateTime.current)
+    #     @device = @devices.update(lending_status: 1)
+    #     if @rental.save
+    #       redirect_to root_path
+    #     else
+    #       render :new
+    #     end
+    #   end
+
+    # else
+    #   flash.now[:alert]= '入力された機器IDは存在しません'
+    #   render :new
+    # end
     
   end
 
